2d71720c91df4a890cfeae988c52af948793e49f
kodjo2022 <107310312+kodjo2022@users.noreply.github.com>
2025-03-02 05:13:11 +0000

    Lab 6 done

--- DiceRoll.py
+++ DiceRoll.py
@@ -8,11 +8,17 @@ def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
-  #find the sum total of the two dice
+  for r in range(100):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    rolls[dice1] = rolls[dice1]
+    rolls[dice2] = rolls[dice2]
+    #find the sum total of the two dice
+    total = dice1 + dice2
+    rolls[total - 2] = rolls[total - 2] + 1
   
   #print statictics for dice rolls
-
-
+  for i in range(11):  
+    print(str(i + 2) + " : " + str(rolls[i]))
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,23 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
-  for line in textFile:
-    print(line)
-  
+  file_name = input("Enter the name of the file: ")
+  with  open(file_name,'r') as textFile:
+  #textFile = open("gettysberg.txt", 'r')
+      lineCount = 0
+      wordCount = 0
+      letterCount = 0
+      charCount = 0
+      for line in textFile:
+        lineCount = lineCount + 1
+        charCount += len(line)
+        words = line.split()
+        for w in words:
+          wordCount = wordCount + 1
 
+  print("Lines: " , lineCount)
+  print("words:", wordCount)
+  print("Characters :", charCount)
+  
 if __name__ == '__main__':
   main()
